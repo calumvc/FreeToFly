b06f2d2ea3d645eca73e3f08dee254870d1b4917
“Lawrence <raf_law@outlook.com>
2025-10-25 17:08:23 +0100

    now only validated path keeps on the canvas

--- modules/pathtrack.js
+++ modules/pathtrack.js
@@ -5,6 +5,10 @@ window.addEventListener("DOMContentLoaded",(e)=>{
     var flag = 0;
     var firstDraw = 0; //get rid of random line from top left
     var lastposition = [0,0];
+    var pathsDic = {};
+    //pathsDic[pathcount(id)][0 for dot records and 1 for drawing][separate records]
+    var pathcount = 0;
+    var currentPath = [];
 
     function singleRecord(event,record){
         var x = event.clientX;
@@ -19,6 +23,7 @@ window.addEventListener("DOMContentLoaded",(e)=>{
             ctx.lineTo(x,y);
             ctx.stroke();
             ctx.closePath();
+            currentPath.push(["line",lastposition[0],lastposition[1],x,y]);
         }
 
         lastposition = [x,y];
@@ -43,10 +48,43 @@ window.addEventListener("DOMContentLoaded",(e)=>{
         ctx.arc(x,y,3,0,2*Math.PI); //3 is the radius in case you wanna change
         ctx.fill();
         ctx.closePath();
+        currentPath.push(["dot",x,y,3,0,2*Math.PI]);
+    }
+
+    function pathComplete(index){
+        delete pathsDic(index);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawValidPath();
+    }
+
+    function addValidPath(){
+        pathsDic[pathcount].push(record);
+        pathsDic[pathcount].push(currentPath);
+    }
+
+    function drawValidPath(){
+        for (let i = 0; i<pathcount; i++){
+            for (let j =0; j<length(pathsDic[pathcount]);j++){
+                if (pathsDic[i][1][j][0]=="line"){
+                    ctx.beginPath();
+                    ctx.moveTo(pathsDic[i][1][j][1],pathsDic[i][1][j][2]);
+                    ctx.lineTo(pathsDic[i][1][j][3],pathsDic[i][1][j][4]);
+                    ctx.stroke();
+                    ctx.closePath();
+                } else if (pathsDic[i][1][j][0]=="dot"){
+                    ctx.beginPath();
+                    ctx.fillStyle = "grey";
+                    ctx.arc(pathsDic[i][1][j][1],pathsDic[i][1][j][2],pathsDic[i][1][j][3],pathsDic[i][1][j][4],pathsDic[i][1][j][5]);
+                    ctx.fill();
+                    ctx.closePath();
+                }
+            }
+        }
     }
 
     canvas.addEventListener('mousedown', (e) =>{
     flag = 1;
+    pathsDic[pathcount] = []
     });
     canvas.addEventListener('mousemove', (e) =>{
     if (flag == 1){
@@ -59,5 +97,10 @@ window.addEventListener("DOMContentLoaded",(e)=>{
     flag = 0;
     firstDraw = 0;
     record=[];
+    console.log(currentPath);
+    currentPath=[];
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawValidPath();
 })
 })
