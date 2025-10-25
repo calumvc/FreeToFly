2217635d3f83d9136ad434254fed4342cdfa5aac
calumvc <calum.cardownie@gmail.com>
2025-10-25 16:22:50 +0100

    World map added

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 //might add another event listener to start game in the future
 import gameLoop from "./modules/gameLoop.js";
+import { placeURL } from "./modules/helpRender.js";
 
-var gameArea = {
+export var gameArea = {
   canvas : document.createElement("canvas"),
   start : function() {
-    this.canvas.width = 1600
-    this.canvas.height = 900
-    this.canvas.id = "canvas"
+    this.canvas.width = 1920;
+    this.canvas.height = 1080;
+    this.canvas.id = "canvas";
     this.context = this.canvas.getContext("2d");
+    placeURL("./assets/world_map.png", this.context, 0, 0);
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+
+    canvas.addEventListener('click', (e) => {
+            const x = e.clientX;
+            const y = e.clientY;
+
+            alert(x + ", " + y);
+        });
+
     gameLoop();
   }
 }
