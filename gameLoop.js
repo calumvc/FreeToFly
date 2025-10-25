4ce84c132ad7406044339173c97d8c79cdd5c4f0
Andrei Boghean <67372068+AndreiBoghean@users.noreply.github.com>
2025-10-25 15:07:23 +0100

    add helpRender.js with placePlane, placeAirport, placeURL

--- gameLoop.js
+++ gameLoop.js
@@ -1,48 +1,14 @@
-var timer = 1; // game timer
-var score = 0; // user's score
-var gaming; // boolean for alive
-var level = 1;
-const LEVELTIME = 20;
 
 export default function gameLoop(){
+  var timer; // timer for the game
+  var score;
+  var gaming; // boolean for alive
 
   setInterval(tick, 1000);
 }
 
-const tick = () => {
-
-  if (gaming === false){ // if player dies
-    clearInterval();
-    gameOver();
-    return;
-  }
-
-  if ((timer % (LEVELTIME / (level * 2)) == 0) || (timer == 2)){
-    spawnMission();
-  }
-
+function tick(){
+  console.log("Hey");
   console.log(timer);
-  if ((timer % LEVELTIME) == 0){
-    level++;
-    console.log("Level incremented");
-    console.log(level);
-  }
   timer++;
-
-  if (timer === 120){
-    gaming = false;
-  }
-
 }
-
-export function gameOver(){
-  console.log("GAME OVER!!");
-}
-
-export function spawnMission(){
-  console.log("Mission from airport A to airport B")
-}
-
-export function spawnAirport(){}
-
-export function spawnPlane(){}
