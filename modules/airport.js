949c3a0fb086cabb3f8b3f6bfd800aa6670366aa
Joshua Niewczas <joshua.niewczas@glasgow.ac.uk>
2025-10-25 16:41:24 +0100

    fixing airport code

--- modules/airport.js
+++ modules/airport.js
@@ -1,30 +1,31 @@
 const airports = {
-  Atlanta: [426, 900-624],
-  Dubai: [1044, 900-582],
-  Dallas: [373, 900-619],
-  Tokyo: [1444, 900-598],
-  London: [800, 900-720],
-  Istanbul: [924, 900-667],
-  Chicago: [413, 900-667],
-  Delhi: [1142, 900-598],
-  Shanghai: [1333, 900-619],
-  California: [271, 900-645],
-  Guangzhou: [1302, 900-571],
-  Seoul: [1360, 900-645],
-  Paris: [808, 900-704],
-  Singapore: [1257, 900-455],
-  Beijing: [1315, 900-656],
-  Amsterdam: [817, 900-725],
-  Madrid: [786, 900-661],
-  NewYork: [471, 900-661],
-  Bangkok: [1244, 900-518],
-  Frankfurt: [835, 900-714],
-  Charlotte: [444, 900-635],
-  LasVegas: [288, 900-640],
-  KualaLumpur: [1248, 900-465],
+  Atlanta: [426, 900 - 624],
+  Dubai: [1044, 900 - 582],
+  Dallas: [373, 900 - 619],
+  Tokyo: [1444, 900 - 598],
+  London: [800, 900 - 720],
+  Istanbul: [924, 900 - 667],
+  Chicago: [413, 900 - 667],
+  Delhi: [1142, 900 - 598],
+  Shanghai: [1333, 900 - 619],
+  California: [271, 900 - 645],
+  Guangzhou: [1302, 900 - 571],
+  Seoul: [1360, 900 - 645],
+  Paris: [808, 900 - 704],
+  Singapore: [1257, 900 - 455],
+  Beijing: [1315, 900 - 656],
+  Amsterdam: [817, 900 - 725],
+  Madrid: [786, 900 - 661],
+  NewYork: [471, 900 - 661],
+  Bangkok: [1244, 900 - 518],
+  Frankfurt: [835, 900 - 714],
+  Charlotte: [444, 900 - 635],
+  LasVegas: [288, 900 - 640],
+  KualaLumpur: [1248, 900 - 465],
 };
 
 export let currentAirports = [];
+let currentAirportNames = [];
 
 export function createAirport(colour) {
   const airportKeys = Object.keys(airports);
@@ -39,7 +40,7 @@ export function createAirport(colour) {
   do {
     randInt = Math.floor(Math.random() * airportKeys.length);
     airportName = airportKeys[randInt];
-  } while (airportName in currentAirports);
+  } while (currentAirportNames.includes(airportName));
 
   const airport = new Object({
     name: airportName,
@@ -47,7 +48,7 @@ export function createAirport(colour) {
     colour: colour,
   });
 
-  console.log(airport.location);
+  currentAirportNames.push(airport.name);
   currentAirports.push(airport);
   return airport;
 }
--- modules/gameLoop.js
+++ modules/gameLoop.js
@@ -1,7 +1,6 @@
-import {placePlane, placeAirport} from "./helpRender.js"; 
-import {createAirport, currentAirports} from "./airport.js"
+import { placePlane, placeAirport } from "./helpRender.js";
+import { createAirport, currentAirports } from "./airport.js";
 import { gameArea } from "../main.js";
-
 
 var timer = 1; // game timer
 var score = 0; // user's score
@@ -10,57 +9,61 @@ var level = 1;
 const LEVELTIME = 20;
 var live_planes = [];
 
-for(let i = 0; i < 25; i++){
+for (let i = 0; i < 25; i++) {
   createAirport("Black");
 }
 
-export default function gameLoop(){
-
+export default function gameLoop() {
   setInterval(tick, 1000);
 }
 
 const tick = () => {
-
-  if (gaming === false){ // if player dies
+  if (gaming === false) {
+    // if player dies
     clearInterval();
     gameOver();
     return;
   }
-  console.log(currentAirports[1]);
-  live_planes.forEach(plane => {
-    placePlane(gameArea.context, plane.CurrentPos[0], plane.CurrentPos[1], plane.rotation);
+
+  live_planes.forEach((plane) => {
+    placePlane(
+      gameArea.context,
+      plane.CurrentPos[0],
+      plane.CurrentPos[1],
+      plane.rotation
+    );
   });
 
-  currentAirports.forEach(airport => {
+  currentAirports.forEach((airport) => {
     placeAirport(gameArea.context, airport.location[0], airport.location[1]);
   });
 
-    if ((timer % Math.round((LEVELTIME / (level * 1.5))) == 0) || (timer == 2)){ // logic to spawn planes, increasing as the level increments{
+  if (timer % Math.round(LEVELTIME / (level * 1.5)) == 0 || timer == 2) {
+    // logic to spawn planes, increasing as the level increments{
     spawnMission();
   }
 
   console.log(timer);
-  if ((timer % LEVELTIME) == 0){
+  if (timer % LEVELTIME == 0) {
     level++;
     console.log("Level incremented");
     console.log(level);
   }
   timer++;
 
-  if (timer === 120){
+  if (timer === 120) {
     gaming = false;
   }
+};
 
-}
-
-export function gameOver(){
+export function gameOver() {
   console.log("GAME OVER!!");
 }
 
-export function spawnMission(){
-  console.log("Mission from airport A to airport B")
+export function spawnMission() {
+  console.log("Mission from airport A to airport B");
 }
 
-export function spawnAirport(){}
+export function spawnAirport() {}
 
-export function spawnPlane(){}
+export function spawnPlane() {}
