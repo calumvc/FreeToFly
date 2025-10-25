82e19b8e2d0b15447d778fb878ff64eaea7a5437
calumvc <calum.cardownie@gmail.com>
2025-10-25 14:47:01 +0100

    Game logic continued

--- gameLoop.js
+++ gameLoop.js
@@ -1,25 +1,48 @@
-var timer = 0; // game timer
+var timer = 1; // game timer
+var score = 0; // user's score
 var gaming; // boolean for alive
+var level = 1;
+const LEVELTIME = 20;
 
 export default function gameLoop(){
-  var score;
 
   setInterval(tick, 1000);
 }
 
 const tick = () => {
 
-  if (gaming === false){
+  if (gaming === false){ // if player dies
     clearInterval();
+    gameOver();
     return;
   }
 
-  console.log("Hey");
+  if ((timer % (LEVELTIME / (level * 2)) == 0) || (timer == 2)){
+    spawnMission();
+  }
+
   console.log(timer);
+  if ((timer % LEVELTIME) == 0){
+    level++;
+    console.log("Level incremented");
+    console.log(level);
+  }
   timer++;
 
-  if (timer === 5){
+  if (timer === 120){
     gaming = false;
   }
 
 }
+
+export function gameOver(){
+  console.log("GAME OVER!!");
+}
+
+export function spawnMission(){
+  console.log("Mission from airport A to airport B")
+}
+
+export function spawnAirport(){}
+
+export function spawnPlane(){}
