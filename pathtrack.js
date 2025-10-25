aefeb0f894090b8e61186dc8baa0fa4c75b5b3e7
“Lawrence <raf_law@outlook.com>
2025-10-25 15:52:15 +0100

    added some lines between drawed dots...Need to find solutions to discard a certain set of operations

--- pathtrack.js
+++ pathtrack.js
@@ -1,51 +1,63 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
-var record = [];
-var flag = 0;
-var lastposition = [0,0];
+window.addEventListener("DOMContentLoaded",(e)=>{
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    var record = [];
+    var flag = 0;
+    var firstDraw = 0; //get rid of random line from top left
+    var lastposition = [0,0];
 
-function singleRecord(event,record){
-    var x = event.clientX;
-    var y = event.clientY;
-    record.push([x,y]);
-    lastposition = [x,y];
-    console.log("position recorded"+lastposition);
-    draw(x,y);
-}
+    function singleRecord(event,record){
+        var x = event.clientX;
+        var y = event.clientY;
+        record.push([x,y]);
 
-function compareDistance(event,lastpos){ //only record after some distance moved
-  currentX = event.clientX;
-  currentY = event.clientY;
-  lastX = lastpos[0];
-  lastY = lastpos[1];
-  if ((currentX - lastX)**2 + (currentY - lastY)**2 > 800){ // modify the number to determine the distance between path dots
-    return 1;
-  }else {
-    return 0;
-  }
-}
+        if (firstDraw == 0){
+            firstDraw = 1;
+        } else {
+            ctx.beginPath();
+            ctx.moveTo(lastposition[0],lastposition[1]);
+            ctx.lineTo(x,y);
+            ctx.stroke();
+            ctx.closePath();
+        }
 
-function draw(x,y){
-    ctx.beginPath();
-    ctx.fillStyle = "grey";
-    ctx.arc(x,y,3,0,2*Math.PI); //3 is the radius in case you wanna change
-    ctx.fill();
-    ctx.closePath();
-}
-
-canvas.addEventListener('mousedown', (e) =>{
-  flag = 1;
-});
-canvas.addEventListener('mousemove', (e) =>{
-  if (flag == 1){
-    if (compareDistance(e,lastposition) == 1){
-      singleRecord(e,record);
+        lastposition = [x,y];
+        drawdot(x,y);
     }
-  }
+
+    function compareDistance(event,lastpos){ //only record after some distance moved
+    currentX = event.clientX;
+    currentY = event.clientY;
+    lastX = lastpos[0];
+    lastY = lastpos[1];
+    if ((currentX - lastX)**2 + (currentY - lastY)**2 > 600){ // modify the number to determine the distance between path dots
+        return 1;
+    }else {
+        return 0;
+    }
+    }
+
+    function drawdot(x,y){
+        ctx.beginPath();
+        ctx.fillStyle = "grey";
+        ctx.arc(x,y,3,0,2*Math.PI); //3 is the radius in case you wanna change
+        ctx.fill();
+        ctx.closePath();
+    }
+
+    canvas.addEventListener('mousedown', (e) =>{
+    flag = 1;
+    });
+    canvas.addEventListener('mousemove', (e) =>{
+    if (flag == 1){
+        if (compareDistance(e,lastposition) == 1){
+        singleRecord(e,record);
+        }
+    }
+    })
+    canvas.addEventListener('mouseup',(e)=>{
+    flag = 0;
+    firstDraw = 0;
+    record=[];
 })
-canvas.addEventListener('mouseup',(e)=>{
-  flag = 0;
-  console.log("mouse position recorded"+record);
-  record=[];
-  console.log("mouse position cleared");
 })
