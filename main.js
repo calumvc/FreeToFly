68a2fe589f4f80f6bb7dd6d7085133e4128cf1ba
calumvc <calum.cardownie@gmail.com>
2025-10-25 12:08:42 +0100

    Base canvas

---
+++ main.js
@@ -0,0 +1,12 @@
+
+var gameArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+    this.canvas.width = 1600
+    this.canvas.height = 900
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+  }
+}
+
+gameArea.start();
